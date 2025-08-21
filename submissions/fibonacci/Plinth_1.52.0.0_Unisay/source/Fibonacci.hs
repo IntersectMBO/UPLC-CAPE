2f2dc89c191e916d2dece10d8cdef4869b069431
Yura Lazaryev <yuriy.lazaryev@iohk.io>
2025-08-21 09:29:20 +0200

    feat(submissions): include source code in Plinth submissions
    Add source code files directly to the Plinth submissions for factorial
    and fibonacci benchmarks. This improves transparency and makes it easier
    to review the implementation details without navigating to the plinth
    library directory.
    
    Update source README files to reference local copies instead of symlinks
    to the plinth library, ensuring self-contained submissions.

---
+++ submissions/fibonacci/Plinth_1.52.0.0_Unisay/source/Fibonacci.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE Strict #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+--
+{-# OPTIONS_GHC -fno-full-laziness #-}
+{-# OPTIONS_GHC -fno-ignore-interface-pragmas #-}
+{-# OPTIONS_GHC -fno-omit-interface-pragmas #-}
+{-# OPTIONS_GHC -fno-spec-constr #-}
+{-# OPTIONS_GHC -fno-specialise #-}
+{-# OPTIONS_GHC -fno-strictness #-}
+{-# OPTIONS_GHC -fno-unbox-small-strict-fields #-}
+{-# OPTIONS_GHC -fno-unbox-strict-fields #-}
+{-# OPTIONS_GHC -fplugin PlutusTx.Plugin #-}
+{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:no-conservative-optimisation #-}
+{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:no-preserve-logging #-}
+{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:remove-trace #-}
+{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:target-version=1.1.0 #-}
+
+module Fibonacci (fibonacciCode, fibonacci25Code) where
+
+import PlutusTx
+import PlutusTx.Prelude
+
+-- | Compiled validator script
+fibonacciCode :: CompiledCode (Integer -> Integer)
+fibonacciCode = $$(PlutusTx.compile [||fibonacci||])
+
+-- | The compiled fibonacci validator for n=25
+fibonacci25Code :: CompiledCode Integer
+fibonacci25Code = fibonacciCode `unsafeApplyCode` liftCodeDef (25 :: Integer)
+
+{-# INLINEABLE fibonacci #-}
+fibonacci :: Integer -> Integer
+fibonacci n
+  | n <= 1 = n
+  | otherwise = fibonacci (n - 1) + fibonacci (n - 2)
